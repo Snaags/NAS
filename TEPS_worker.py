Added softmax, fixed normal layers so they remain constant in dimentions, adding logging of data dims through model, fixed error with batch normalisation when doing FactorizedReduce

TEPS_worker.py
@@ -120,8 +120,8 @@ class Test_TEPS(TEPS):
     super().__init__(window_size, "x_test.npy","y_test.npy")
     
 
-def main(hyperparameter,budget = 300):
-    VISUAL_MODE = False
+def main(hyperparameter,budget = 2000):
+    VISUAL_MODE = True
     def cal_acc(y,t):
         return np.count_nonzero(y==t)/len(y)
     def convert_label_max_only(y):
@@ -158,7 +158,7 @@ def main(hyperparameter,budget = 300):
     n_iter = train_dataset.get_n_samples()/batch_size
     if max_iter < n_iter:
       n_iter = max_iter
-    epochs = int(2)
+    epochs = int(1)
     optimizer = torch.optim.Adam(model.parameters(),lr = hyperparameter["lr"])
     criterion = nn.CrossEntropyLoss()
     acc = 0
@@ -225,7 +225,6 @@ def main(hyperparameter,budget = 300):
 
 
 if __name__ == "__main__":
-  hyperparameter = {'channels': 32, 'convcell_1_num_ops': 5, 'convcell_1_ops_1_input_1': 0, 'convcell_1_ops_1_input_2': 0, 'convcell_1_ops_1_type': 'Conv5', 'convcell_1_ops_2_input_1': 1, 'convcell_1_ops_2_input_2': 1, 'convcell_1_ops_2_type': 'StdConv', 'convcell_1_ops_3_input_1': 2, 'convcell_1_ops_3_input_2': 0, 'convcell_1_ops_3_type': 'AvgPool', 'convcell_1_ops_4_input_1': 2, 'convcell_1_ops_4_input_2': 2, 'convcell_1_ops_4_type': 'MaxPool', 'convcell_1_ops_5_input_1': 4, 'convcell_1_ops_5_input_2': 2, 'convcell_1_ops_5_type': 'StdConv', 'layers': 3, 'lr': 0.010326044660341144, 'num_conv': 1, 'num_re': 1, 'reducell_1_num_ops': 1, 'reducell_1_ops_1_input_1': 0, 'reducell_1_ops_1_input_2': 0, 'reducell_1_ops_1_type': 'FactorizedReduce', 'window_size': 325}
-
-  main(hyperparameter,20000 )
+  hyperparameter = {'channels': 36, 'layers': 4, 'lr': 0.00017257004865709992, 'normal_cell_1_num_ops': 1, 'normal_cell_1_ops_1_input_1': 0, 'normal_cell_1_ops_1_input_2': 0, 'normal_cell_1_ops_1_type': 'Conv3', 'normal_cell_1_ops_2_input_1': 1, 'normal_cell_1_ops_2_input_2': 0, 'normal_cell_1_ops_2_type': 'Conv3', 'normal_cell_1_ops_3_input_1': 2, 'normal_cell_1_ops_3_input_2': 2, 'normal_cell_1_ops_3_type': 'Conv3', 'normal_cell_1_ops_4_input_1': 1, 'normal_cell_1_ops_4_input_2': 3, 'normal_cell_1_ops_4_type': 'Conv3', 'normal_cell_1_ops_5_input_1': 2, 'normal_cell_1_ops_5_input_2': 1, 'normal_cell_1_ops_5_type': 'Conv5', 'normal_cell_1_ops_6_input_1': 5, 'normal_cell_1_ops_6_input_2': 5, 'normal_cell_1_ops_6_type': 'Conv5', 'normal_cell_1_ops_7_input_1': 3, 'normal_cell_1_ops_7_input_2': 5, 'normal_cell_1_ops_7_type': 'AvgPool', 'normal_cell_1_ops_8_input_1': 2, 'normal_cell_1_ops_8_input_2': 1, 'normal_cell_1_ops_8_type': 'Conv5', 'normal_cell_1_ops_9_input_1': 6, 'normal_cell_1_ops_9_input_2': 3, 'normal_cell_1_ops_9_type': 'MaxPool', 'num_conv': 1, 'num_re': 1, 'p': 0.05, 'reduction_cell_1_num_ops': 1, 'reduction_cell_1_ops_1_input_1': 0, 'reduction_cell_1_ops_1_input_2': 0, 'reduction_cell_1_ops_1_type': 'FactorizedReduce', 'window_size': 134}
+  main(hyperparameter,5000 )
 

model_constructor.py
@@ -1,19 +1,38 @@
 import ops1d as ops
+import random
 #hyperparameter processing
 from operator import itemgetter 
 import torch.nn as nn
 
+class DataShapeLogger:
+  def __init__(self, filename):
+    self.filename = filename
+    self.log_list = [] 
+  def log(self, *string):
+    string = [str(x) for x in string ]
+    self.log_list.append("".join(string))
+  def write(self):   
+    MyFile=open(self.filename+".txt",'w')
+    
+    for element in self.log_list:
+         MyFile.write(element)
+         MyFile.write('\n')
+    MyFile.close()
 
 class Model(nn.Module):
   def __init__(self, input_size, output_size, hyperparameters):
     super(Model,self).__init__()
+    self.log_flag = True
+    self.logger = DataShapeLogger("logger.txt")
     self.hyperparameters = hyperparameters  
     self.channels = hyperparameters["channels"]
-    self.cells = nn.ModuleList()
+    self.p = hyperparameters["p"]
     self.build_cells(hyperparameters)
     self.in_conv = ops.StdConv(input_size[0], self.channels)
+    self.layers = hyperparameters["layers"]
     self.gap = ops.AdaAvgPool() 
     self.fc = nn.Linear(self.channels, output_size)
+    self.fcact = nn.Softmax(dim = 1)
   def _build_dict(self,parameters : dict, keyword : str):
     _dictionary = dict()
     keyword_length = len(keyword)
@@ -35,36 +54,59 @@ class Model(nn.Module):
     return _dictionary
   
   def build_cells(self, parameters): 
-    conv_dictionary = self._build_dict(parameters, "convcell")
-    redu_dictionary = self._build_dict(parameters, "reducell")
-    for i in range(parameters["layers"]):
-      self.cells.append(Cell(conv_dictionary[1],self.channels))
-      if i != (parameters["layers"] -1):
-        self.cells.append(Cell(redu_dictionary[1],self.channels))
-
-
-  def forward(self,x):
+    conv_dictionary = self._build_dict(parameters, "normal_cell")
+    redu_dictionary = self._build_dict(parameters, "reduction_cell")
+    self.cellsconv = Cell(conv_dictionary[1],self.channels,self.p)
+    self.cellsredu = Cell(redu_dictionary[1],self.channels,self.p)
+  
+  def _forward(self,x):
     x = self.in_conv(x)
-    for i in self.cells:
-      x = i(x)
-    #print("Size of x after cells: ", x.size())
+    for i in range(self.layers):
+      x = self.cellsconv(x) 
+      if i != (self.layers -1):
+        x = self.cellsredu(x)
     x = self.gap(x)
-    #print("Size of x after gap: ", x.size())
-    #print("Size of dense input: ", self.channels)
-
     x = self.fc(x.squeeze())
+    x = self.fcact(x)
     return x  
 
+  def _forward_log(self,x):
+    self.logger.log("Input Size: ", x.size())
+    x = self.in_conv(x)
+    self.logger.log("After in_conv: ", x.size() )
+    for i in range(self.layers):
+      x = self.cellsconv(x) 
+      self.logger.log("Data after Normal Cell ", str(i),": ",x.size())
+      if i != (self.layers -1):
+        x = self.cellsredu(x)
+        self.logger.log("Data after Reduction Cell ", str(i),": ",x.size())
+    self.logger.log("Size of x after cells: ", x.size())
+    x = self.gap(x)
+    self.logger.log("Size of x after gap: ", x.size())
+    self.logger.log("Size of dense input: ", self.channels)
+    x = self.fc(x.squeeze())
+    x = self.fcact(x)
+    self.logger.write()
+    return x  
 
+  def forward(self,x):
+    if self.log_flag == True:
+      self.log_flag = False
+      return self._forward_log(x)
+    else:
+      return self._forward(x)
 class Ops(nn.Module):
-  def __init__(self, parameters, channels):
+  def __init__(self, parameters, channels, p):
     super(Ops,self).__init__()
     self.args = {}
     self.channels = channels
     self.multicompute = False
+    self.p = p
     self.input = []
+    self.dropout = nn.Dropout(p = 0.2)
     for i in parameters:
       if i == "type":
+        self.op = parameters[i]
         self.operation = self.get_operation(parameters[i])
       elif i[:-2] == "input":
           if parameters[i] not in self.input:
@@ -75,6 +117,7 @@ class Ops(nn.Module):
     if len(self.input) > 1:
       self.compute.append(ops.StdAdd())
       self.multicompute = True
+      self.pool = nn.AvgPool1d(2)
     self.compute.append(self.operation(**self.args))
   def get_required(self) -> list:
     return self.input
@@ -82,29 +125,30 @@ class Ops(nn.Module):
   def get_operation(self, op_key):
     if op_key == "StdConv":
       operation = ops.StdConv
-      self.args["C_in"] = self.args["C_out"] = self.channels 
+      self.args["C_in"] = self.args["C_out"] = self.channels
+      self.args["padding"] = "same" 
     elif op_key == "Conv3":
       operation = ops.ConvBranch
       self.args["kernel_size"] = 3
       self.args["stride"] = 1
-      self.args["padding"] = 0
+      self.args["padding"] = "same"
       self.args["separable"] = False
       self.args["C_in"] = self.args["C_out"] = self.channels 
     elif op_key == "Conv5":
       operation = ops.ConvBranch
       self.args["kernel_size"] = 5
       self.args["stride"] = 1
-      self.args["padding"] = 0 
+      self.args["padding"] = "same" 
       self.args["separable"] = False
       self.args["C_in"] = self.args["C_out"] = self.channels 
     elif op_key == "MaxPool":    
       operation = ops.Pool
       self.args["pool_type"] = "max"
-      self.args["kernel_size"] = 3
+      self.args["kernel_size"] = 5
     elif op_key == "AvgPool":    
       operation = ops.Pool
       self.args["pool_type"] = "avg"
-      self.args["kernel_size"] = 3
+      self.args["kernel_size"] = 5
     elif op_key == "FactorizedReduce":
       operation = ops.FactorizedReduce
       self.args["C_in"] = self.args["C_out"] = self.channels 
@@ -112,12 +156,19 @@ class Ops(nn.Module):
       operation = ops.StdConv
     return operation
   def forward( self, x ):
-
+    #print(self.op)
     for count,i in enumerate(self.compute):
       if self.multicompute and count == 0:
-        x = i(*x)  
+        #print("Size Before operation: ", x[0].size(),x[1].size())
+        x = i(*x) 
+        #print("Size After operation: ", x.size())
       else:
+        x = self.dropout(x)
+        #print("Size Before operation: ", x.size())
         x = i(x)
+        #print("Size After operation: ", x.size())
+    if self.multicompute:
+      x = self.pool(x)
     return x 
   
   def process( self, x : list):
@@ -127,10 +178,11 @@ class Cell(nn.Module):
   """
   Contains a series of operations and information links
   """
-  def __init__(self,parameters,channels):
+  def __init__(self,parameters,channels,p):
     super(Cell, self).__init__() 
     self.ops_id = [] #numerical identifier for each operation 
     self.ops = []
+    self.p = p
     self.channels = channels
     self.inputs = []
     self.output_operation = parameters["num_ops"]
@@ -160,7 +212,7 @@ class Cell(nn.Module):
     ops_dictionary = self._build_dict(parameters, "op")  
     for i in ops_dictionary:
       
-      self.ops.append(Ops(ops_dictionary[i], self.channels))
+      self.ops.append(Ops(ops_dictionary[i], self.channels, self.p))
       self.ops_id.append(i)
   def calculate_compute_order(self):
     #

ops.py
@@ -89,7 +89,7 @@ class Pool(nn.Module):
     def __init__(self, pool_type, kernel_size, stride = None, padding = 0):
         super().__init__()
         if stride == None:
-          stride = kernel_size
+          stride = 1
         if pool_type.lower() == 'max':
             self.pool = nn.MaxPool2d(kernel_size, stride, padding)
         elif pool_type.lower() == 'avg':

ops1d.py
@@ -10,10 +10,10 @@ def min_which(a : int, b : int):
     return b, 1
 
 class StdConv(nn.Module):
-    def __init__(self, C_in, C_out):
+    def __init__(self, C_in, C_out,padding = 0):
         super(StdConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv1d(C_in, C_out, 1, stride=1, padding=0, bias=False),
+            nn.Conv1d(C_in, C_out, 1, stride=1, padding=padding, bias=False),
             nn.BatchNorm1d(C_out, affine=False),
             nn.ReLU()
         )
@@ -101,10 +101,10 @@ class FactorizedReduce(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv1d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
         self.conv2 = nn.Conv1d(C_in, C_out // 2, 1, stride=2, padding=0, bias=False)
-        self.bn = nn.BatchNorm1d(C_out, affine=affine)
+        self.bn = nn.BatchNorm1d(int(C_out/2)*2, affine=affine)
 
     def forward(self, x):
-        out = torch.cat([self.conv1(x), self.conv2(x[:,:,1:])], dim=1)
+        out = torch.cat([self.conv1(x), self.conv2(x[:,:,:])], dim=1)
         out = self.bn(out)
         return out
 
@@ -113,11 +113,12 @@ class Pool(nn.Module):
     def __init__(self, pool_type, kernel_size, stride = None, padding = 0):
         super().__init__()
         if stride == None:
-          stride = kernel_size
+          stride = 1
+           
         if pool_type.lower() == 'max':
-            self.pool = nn.MaxPool1d(kernel_size, stride, padding)
+            self.pool = nn.MaxPool1d(kernel_size, stride, padding = 2, ceil_mode = True)
         elif pool_type.lower() == 'avg':
-            self.pool = nn.AvgPool1d(kernel_size, stride, padding, count_include_pad=False)
+            self.pool = nn.AvgPool1d(kernel_size, stride, padding = 2, count_include_pad=False,ceil_mode= True)
         else:
             raise ValueError()
 
@@ -156,7 +157,7 @@ class DropPath(nn.Module):
         if self.training and self.p > 0.:
             keep_prob = 1. - self.p
             # per data point mask
-            mask = torch.zeros((x.size(0), 1, 1, 1), device=x.device).bernoulli_(keep_prob)
+            mask = torch.zeros((x.size(0), 1, 1), device=x.device).bernoulli_(keep_prob)
             return x / keep_prob * mask
 
         return x